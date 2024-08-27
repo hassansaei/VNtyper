2857164fef0e7071ae9c648eb1158edac653ac0a
Bernt Popp <bernt.popp@gmail.com>
2024-08-27 16:25:27 +0200

    Refactor: Replace Tools with Similar Functions to Reduce Dependencies #3 Feature: Replace print Statements with Logging #1
    - Replaced Picard with Samtools for BAM sorting and indexing in `alignment_processing.py`.
    - Refactored the alignment process to use `samtools view` and `samtools sort` for SAM to BAM conversion and sorting.
    - Replaced all `print` statements with `logging` throughout the codebase, ensuring consistent logging levels and formats.
    - Integrated configuration options for tool paths and processing parameters from `config.json`.
    - Ensured that all processing steps (BWA alignment, BAM cleanup, genotyping) are logged appropriately, improving pipeline traceability and debugging.
    Closes #3
    Closes #1

--- vntyper/scripts/advntr_genotyping.py
+++ vntyper/scripts/advntr_genotyping.py
@@ -1,13 +1,14 @@
 import subprocess as sp
+import logging
 import pandas as pd
 
 def run_advntr(reference, db_file_hg19, sorted_bam, output, output_name):
     advntr_command = f"advntr genotype -fs -vid 25561 --outfmt vcf --alignment_file {sorted_bam} -o {output}{output_name}_adVNTR.vcf " \
                      f"-m {db_file_hg19} -r {reference} --working_directory {output}"
-    print("Launching adVNTR genotyping!\n")
+    logging.info("Launching adVNTR genotyping!")
     process = sp.Popen(advntr_command, shell=True)
     process.wait()
-    print("adVNTR genotyping of MUC1-VNTR done!")
+    logging.info("adVNTR genotyping of MUC1-VNTR done!")
 
 def process_advntr_output(vcf_path):
     # Process adVNTR results
