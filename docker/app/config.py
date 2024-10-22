d47a31577010ce40e1008a9abb08a07f80953cb1
Bernt Popp <bernt.popp@gmail.com>
2024-10-22 10:07:35 +0200

    Refactor: Dockerfile and entrypoint script for improved efficiency and maintainability

--- docker/app/config.py
+++ docker/app/config.py
@@ -1,8 +1,19 @@
 # docker/app/config.py
 
+import os
+import logging
+
 class Settings:
     PROJECT_NAME: str = "VNtyper API"
-    PROJECT_VERSION: str = "2.0.0-alpha.6"
+    PROJECT_VERSION: str = "2.0.0-alpha.7"
     DEBUG: bool = True
 
+    # Logging configuration
+    LOG_LEVEL: str = os.getenv("LOG_LEVEL", "INFO")
+    LOG_FORMAT: str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
 settings = Settings()
+
+# Configure logging
+logging.basicConfig(level=settings.LOG_LEVEL, format=settings.LOG_FORMAT)
+logger = logging.getLogger(__name__)
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -e
 
+# Source conda to ensure the environment is activated
+source /opt/conda/etc/profile.d/conda.sh
+
 # If the first argument is 'vntyper', execute VNtyper locally
 if [ "$1" = "vntyper" ]; then
   exec conda run -n vntyper "$@"
+# If the first argument is 'celery', execute Celery worker
+elif [ "$1" = "celery" ]; then
+  exec conda run -n vntyper celery -A app.celery_worker.celery_app worker --loglevel=info
 else
   # Otherwise, start the FastAPI web service
-  exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
+  exec conda run -n vntyper uvicorn app.main:app --host 0.0.0.0 --port 8000
 fi
