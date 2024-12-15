e636b8c989d661170bdef439a02af1c546991ecc
Hassan Saei <hassan.saei@inserm.fr>
2024-12-15 17:36:56 +0100

    Key error: vid for advntr genotyping

--- vntyper/modules/advntr/advntr_genotyping.py
+++ vntyper/modules/advntr/advntr_genotyping.py
@@ -45,7 +45,7 @@ def run_advntr(db_file, sorted_bam, output, output_name, config):
     output_ext = ".vcf" if output_format == "vcf" else ".tsv"
 
     advntr_command = (
-        f"{advntr_path} genotype -fs -vid {advntr_settings['vid']} "
+        f"{advntr_path} genotype -fs -vid {advntr_settings.get("vid", 25561)} "
         f"--alignment_file {sorted_bam} -o {output}/{output_name}_adVNTR{output_ext} "
         f"-m {db_file} --working_directory {output} -t {threads} {additional_commands}"
     )
